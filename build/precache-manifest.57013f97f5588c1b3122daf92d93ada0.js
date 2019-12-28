01b216a1546da228a7ae26f876a3122a434ffee2
cahaug <caleahaug@gmail.com>
2019-12-28 05:53:07 -0700

    ran build

---
+++ build/precache-manifest.57013f97f5588c1b3122daf92d93ada0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82a0326d18b1a78026f78c415db4ff4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5c840d1c26a3e8f4eff",
+    "url": "/static/css/main.70817aaf.chunk.css"
+  },
+  {
+    "revision": "6661879f743e4c4317f2",
+    "url": "/static/js/2.231b49b2.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.231b49b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5c840d1c26a3e8f4eff",
+    "url": "/static/js/main.3b94e95a.chunk.js"
+  },
+  {
+    "revision": "312f2c3b5a93f9061708",
+    "url": "/static/js/runtime-main.0c704f21.js"
+  }
+]);
