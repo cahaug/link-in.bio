d4e56170a636b354282dc52758382fdf3ce2b565
cahaug <caleahaug@gmail.com>
2020-04-11 19:14:04 -0700

    photo coming through now, giving space and classname

--- src/components/ListDisplay.js
+++ src/components/ListDisplay.js
@@ -45,7 +45,7 @@ class ListDisplay extends React.Component {
 
                             <div className='signup' key={link.entryId}>
                                 <a href={`http://link-in-bio.herokuapp.com/s/?eid=${link.entryId}&ref=${link.referencingURL}`}>{link.linkTitle}</a> <br />
-                                <img src={link.imgURL} alt={link.imgURL} /><br />
+                                <img className='image' src={link.imgURL} alt={link.imgURL} /><br />
                                 <p>{link.description}</p>
                             </div>
 
