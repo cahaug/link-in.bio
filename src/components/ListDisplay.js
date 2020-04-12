d2cee3ce1c62441bacc058f37d7eee5cdb96df5e
cahaug <caleahaug@gmail.com>
2020-04-11 19:18:54 -0700

    aesthetic decision moved linktitle below linkimg

--- src/components/ListDisplay.js
+++ src/components/ListDisplay.js
@@ -44,8 +44,8 @@ class ListDisplay extends React.Component {
                     return (
 
                             <div className='signup' key={link.entryId}>
-                                <a href={`http://link-in-bio.herokuapp.com/s/?eid=${link.entryId}&ref=${link.referencingURL}`}>{link.linkTitle}</a> <br />
                                 <img className='image' src={link.imgURL} alt={link.imgURL} /><br />
+                                <a href={`http://link-in-bio.herokuapp.com/s/?eid=${link.entryId}&ref=${link.referencingURL}`}>{link.linkTitle}</a> <br />
                                 <p>{link.description}</p>
                             </div>
 
