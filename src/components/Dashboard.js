accb5b8b27dcd835a8c9f61f5d666a5cc60b1f92
cahaug <caleahaug@gmail.com>
2020-04-28 13:06:07 -0700

    smooth registration and 2nd list creation now smile

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -72,14 +72,14 @@ class Dashboard extends React.Component {
                 <p>Your List Views is {this.state.isLoadingListViews ? <span> Loading...</span> : this.state.listViews}</p>
                 <p>To get started, create a list, then add your entries!</p>
                 <p>Your LinkList will be hosted at: <a alt="Your LinkList" href={`http://link-in.bio/${localStorage.getItem('listId')}`}>http://link-in.bio/{localStorage.getItem('listId')}</a></p>
-                <a href="#neworder" className="abutton" role="button">Create a New List</a>
+                {/* <a href="#neworder" className="abutton" role="button">Create a New List</a>
                 <div className="modal" id="neworder">
                     <div className="modal-container">
-                        <CreateList />
+                        <CreateList /> */}
                         {/* eslint-disable-next-line */}
-                        <a href="#" className="abutton2" role="button">Close</a>
+                        {/* <a href="#" className="abutton2" role="button">Close</a>
                     </div>
-                </div>
+                </div> */}
                 <br />
                 <br />
                 {/* <a href="#neworder2" className="abutton" role="button">Get List Id</a>
