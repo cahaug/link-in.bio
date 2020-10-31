307e1a64522763afe2dfbdfbede7ca54ee7b0bcb
Cale Haug <caleahaug@gmail.com>
2020-10-30 22:50:49 -0700

    fix zoomies on mobile device

--- src/components/SettingsPanel.js
+++ src/components/SettingsPanel.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
-import axios from "axios"
-import loadingGif from '../files/loading.gif'
-import { Link } from 'react-router-dom'
+// import axios from "axios"
+// import loadingGif from '../files/loading.gif'
+// import { Link } from 'react-router-dom'
 // import CUPicker from '../components/CustomURL/CUPicker'
 import BackgroundColorPicker from '../components/BackgroundColorPicker'
 import TextColorPicker from '../components/TextColorPicker'
