6d533942501984249beab42153df7d590f81f3ad
Cale Haug <caleahaug@gmail.com>
2020-12-09 10:20:56 -0700

    hopefully this works!

---
+++ src/components/GraphForHomepage.js
@@ -0,0 +1,147 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import '../App.css'
+import { VictoryPie, VictoryChart, VictoryAxis, VictoryBar, VictoryLine, VictoryTheme, VictoryLabel } from 'victory'
+
+
+const GraphForHomepage = () => {
+    const [isLoading, setIsLoading] = useState()
+    const [datasetBravo, setDatasetBravo] = useState({
+        browserNameCounts:[],
+        countries:[],
+        deviceTypes:[],
+        regions:[],
+        isTouchDevice:[],
+        osFamilyCount:[],
+        deviceOwnNamesCount:[],
+        deviceBrandNamesCount:[],
+        timeline:[]
+    })
+
+   
+    const getDatasetBravo = () => {
+        axios.get('https://link-in-bio.herokuapp.com/s/steakSauce')
+        .then(res => {
+            console.log('res.data bravo', res.data)
+            setDatasetBravo(res.data)
+
+            setIsLoading(false)
+        })
+        .catch(err => {
+            console.log('error in get piegraph', err)
+        })
+    }
+
+    useEffect(()=>{
+        if(datasetBravo.browserNameCounts.length === 0){
+            getDatasetBravo()
+        } else{
+            return
+        }
+    })
+
+    if(isLoading===true){
+        return <p>📝 Drawing Graph...</p>
+    } else {
+        return(
+            <div>
+                <br />
+                <p>List Viewer Location {`&`} Device Information:</p>
+                <div>
+                    <div className="vicLine">
+                        <h2>Daily Viewers:</h2>
+                        <VictoryChart theme={VictoryTheme.material} padding={{bottom:75, left:50,right:50}}>
+                            <VictoryLine data={datasetBravo.timeline} style={{
+                                data: { stroke: "#c43a31" ,}, 
+                                tickLabels:{angle:45,}, 
+                                parent: { border: "1px solid #ccc"}}} scale={{x:"time", y:"linear"}} />
+                            <VictoryAxis dependentAxis />
+                            <VictoryAxis tickFormat={(t) => {const out = t.toString().slice(0,10); return `${out}`}} style={{tickLabels:{angle:45, textAnchor:"start", }, labels:{padding:-20}}} />
+                        </VictoryChart>
+                    </div>
+                </div>
+                <div className="entryChartHolder">
+                    <div className="vicPie">
+                        <h2>Viewer Device Category</h2>
+                        <VictoryPie animate={{duration:3000}}  data={datasetBravo.deviceTypes} colorScale="qualitative" x='deviceType' y='count' style={{
+                            labels: {
+                                fontSize: 25, fill: '#929292'
+                              }
+                        }} />
+                    </div>
+                    <div className="vicPie">
+                        <h2>Browsers Used By Viewers</h2>
+                        <VictoryPie animate={{duration:3000}}  data={datasetBravo.browserNameCounts} colorScale="qualitative" x='browserName' y='count' style={{
+                            labels: {
+                                fontSize: 25, fill: '#929292'
+                              }
+                        }} />
+                    </div>
+                </div>
+                {/* <br /> */}
+                <div className="entryChartHolder">
+                    <div className="vicPie">
+                        <h2>Do Your Viewers Have Touchscreen?</h2>
+                        <VictoryPie animate={{duration:3000}} data={datasetBravo.isTouchDevice} colorScale="qualitative" x='isMobileDevice' y='count' style={{
+                            labels: {
+                                fontSize: 30, fill: '#929292'
+                              }
+                        }} />
+                    </div>
+                    <div className="vicPie">
+                        <h2>Operating System of Viewer</h2>
+                        <VictoryPie animate={{duration:3000}} data={datasetBravo.osFamilyCount} colorScale="qualitative" x='osFamily' y='count' style={{
+                            labels: {
+                                fontSize: 30, fill: '#929292'
+                              }
+                        }} />
+                    </div>
+                </div>
+
+                <div className="entryChartHolder">
+                    <div className="vicPie">
+                        <h2>Brand of Device</h2>
+                        <VictoryPie animate={{duration:3000}} data={datasetBravo.deviceBrandNamesCount} colorScale="qualitative" x='deviceBrandName' y='count' style={{
+                            labels: {
+                                fontSize: 20, fill: '#929292'
+                              }
+                        }} />
+                    </div>
+                    <div className="vicPie">
+                        <h2>Name of Device</h2>
+                        <VictoryPie animate={{duration:3000}} data={datasetBravo.deviceOwnNamesCount} colorScale="qualitative" x='deviceOwnName' y='count' style={{
+                            labels: {
+                                fontSize: 20, fill: '#929292'
+                              }
+                        }} />
+                    </div>
+                </div>
+
+                <div className="entryChartHolder">
+                    <div className="vicPie">
+                        <h2>Country Your Viewers Are From</h2>
+                        <VictoryPie animate={{duration:3000}} data={datasetBravo.countries} colorScale="qualitative" x='countryOfOrigin' y='count' style={{
+                            labels: {
+                                fontSize: 30, fill: '#929292'
+                              }
+                        }} />
+                    </div>
+                    <div className="vicPie">
+                        <h2>Best Guess Location</h2>
+                        <VictoryPie animate={{duration:3000}} data={datasetBravo.regions} colorScale="qualitative" x='province' y='count' style={{
+                            labels: {
+                                fontSize: 20, fill: '#929292'
+                              }
+                        }} />
+                    </div>
+                </div>
+                {/* <br /> */}
+                
+            </div>
+        )
+    }
+
+
+}
+
+export default GraphForHomepage
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import libIMG from '../files/libIMG.png'
 import HomepageAvailability from './CustomURL/HomepageAvailability'
+import GraphForHomepage from './GraphForHomepage'
 // import '../App2.css';
 
 
 const LandingPage = () => {
+    const [isShowingStats, setIsShowingStats] = useState(false)
+    
+    const statsDrawerToggle = () => {
+        const statDrawer = document.getElementsByClassName('statsDisplayDiv')
+        if(statDrawer[0].style.maxHeight){
+            statDrawer[0].style.maxHeight = null;
+            setIsShowingStats(false)
+        } else {
+            statDrawer[0].style.maxHeight = statDrawer[0].scrollHeight + 100 + "px";
+            setIsShowingStats(true)
+        }
+    }
+
     return (
         <div>
             <img src={libIMG} alt="Link-In.Bio Logo" className="landingIMG"/>
             <br />
             <HomepageAvailability />
+            <br />
+            {isShowingStats ? <span onClick={statsDrawerToggle}>Hide Statistics  ▲</span>:<span onClick={statsDrawerToggle}>Your List Stats  ▼</span>}
+            <div className="statsDisplayDiv">
+                <GraphForHomepage />
+                {isShowingStats ? <span onClick={statsDrawerToggle}>Hide Statistics  ▲</span>:<span onClick={statsDrawerToggle}>Your List Stats  ▼</span>}
+
+            </div>
             <br />
             <div className='signupcards'>
                 <Link to='/register' style={{ textDecoration: 'none', color: 'black' }}>
