01eb7734275aece84ce029c532df7a369b12c803
Cale Haug <caleahaug@gmail.com>
2020-12-21 17:02:28 -0700

    boot intercom later to resolve multiple list trackings

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -24,14 +24,14 @@ const LandingPage = () => {
 
     useEffect(() => {
         if(loggedViewNoIP === false){
-            window.Intercom("boot", {
-                app_id: "ya321a09"
-              });
             const mt = navigator.maxTouchPoints
             axios.get(`https://link-in-bio.herokuapp.com/s/hpA1?mt=${mt}`)
             .then(res => {
                 console.log(res.data.message)
                 setLoggedViewNoIP(true)
+                window.Intercom("boot", {
+                    app_id: "ya321a09"
+                });
             })
             .catch(err => {
                 console.log('error', err)
