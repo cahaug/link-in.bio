a01da06c8249e685e10ef607864bbe54a2522fd6
Cale Haug <caleahaug@gmail.com>
2021-01-05 11:39:55 -0700

    hopefully that brings accessibility to one hunnid

--- src/components/ListDisplayHooks.js
+++ src/components/ListDisplayHooks.js
@@ -320,7 +320,7 @@ function ListDisplayHooks(match) {
                         </div>
                         {/* <img src={profilePictureURL} alt={profilePictureURL} style={{width:"200px"}}/>  */}
                         <div className="drawer">
-                            <h4> ©{new Date().getFullYear()}  <a href="http://link-in.bio/"><span className="footerLink"> Link-in.Bio Ltd</span></a></h4>
+                            <h2> ©{new Date().getFullYear()}  <a href="http://link-in.bio/"><span className="footerLink"> Link-in.Bio Ltd</span></a></h2>
                         </div>
                     </div>
                 {/* <hr /> */}
