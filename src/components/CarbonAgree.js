b01037e825b90df9aaa6cf0fba4dd91d6c127831
Cale Haug <caleahaug@gmail.com>
2021-02-14 20:47:34 -0700

    completely restructure homepage

---
+++ src/components/CarbonAgree.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CarbonAgree = () => {
+    return (
+        <div>
+            <br />
+            <h3>Corporations need to do their part to protect the planet.</h3> <br />
+            <p>Link-In Bio pledges to be a Carbon-Negative Corporation, meaning:<p>By subscribing to Link-In Bio, your money will be used to make a positive impact on the environment.</p></p><br />
+            <p>Don't let your digital life be the weakest link in your sustainable business or lifestyle.<p>Subscribe to Link-In Bio, and help build a Greener Future for Everyone.</p></p><br />
+            <br />
+            <p>Link-In.Bio Ltd intends to be fully transparent as well as open to subscriber input in its environmental action,<p>and will show progress through official company Social Media accounts, links coming soon.</p></p>
+        </div>
+    )
+}
+
+export default CarbonAgree
