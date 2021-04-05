f16f6b3dcabdbac4dd958bee4fcabdc41de6e3f4
Cale Haug <caleahaug@gmail.com>
2021-04-05 09:57:54 -0700

    broke overnight...? всё уже отличноё

--- src/components/ListDisplayHooks.js
+++ src/components/ListDisplayHooks.js
@@ -266,7 +266,7 @@ function ListDisplayHooks(match) {
                                 res.data[z].referencingURL = correctedLink
                                 const trashRequest3 = axios.get(`https://link-in-bio.limited/s/?eid=${res.data[z].entryId}&ref=${DOMPurify.sanitize(res.data[z].referencingURL.slice(17))}&mt=${mt}&red=f`)
                                 sessionStorage.setItem(`libViewID:${res.data[z].entryId}`, true)
-                                window.location.href = DOMPurify.sanitize(res.data[z].referencingURL.slice(17))
+                                return window.location.href = DOMPurify.sanitize(res.data[z].referencingURL.slice(17))
                             }
                         } else {
                             const correctedLink = res.data[z].referencingURL.slice(17) 
