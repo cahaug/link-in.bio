dc03f13f42334b913aa416f4d9a7abbd4dc7c901
Cale Haug <caleahaug@gmail.com>
2021-07-01 11:32:48 -0700

    reframe argument

--- src/components/CarbonAgree.js
+++ src/components/CarbonAgree.js
@@ -6,10 +6,10 @@ const CarbonAgree = () => {
             <br />
             <p>As of February 22, 2021, Link-In.Bio Ltd's Backend Servers are powered 100% by Renewable Energy Sources.</p><br />
             <h3>Corporations need to do their part to protect the planet.</h3> <br />
-            <p>Link-In Bio pledges to be a Carbon-Negative Corporation, meaning:</p><p>By subscribing to Link-In Bio, your money will be used to make a positive impact on the environment.</p><br />
+            <p>Link-In Bio pledges to be a Carbon-Neutral Corporation, meaning:</p><p>By subscribing to Link-In Bio, your money spent will not negatively impact the environment.</p><br />
             <p>Don't let your digital life be the weakest link in your sustainable business or lifestyle.</p><p>Subscribe to Link-In Bio, and help build a Greener Future for Everyone.</p><br />
             <br />
-            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/r3U1cvPOc5w" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            {/* <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/r3U1cvPOc5w" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
         </div>
     )
 }
